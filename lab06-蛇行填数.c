1a59ac95a541d60cb5252db23448e1d958ca425f
sinkingwen <50895933+sinkingwen@users.noreply.github.com>
2023-10-21 13:33:18 +0800

    Update lab06-蛇行填数.c

--- lab06-蛇行填数.c
+++ lab06-蛇行填数.c
@@ -40,3 +40,29 @@ int main() {
 	}
 	return 0;
 }
+
+//第二种方式
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int a[15][15];
+int main()
+{
+	//freopen("9.out", "w", stdout); 
+	int n,k=1,x=1,y=0;;
+	cin>>n;
+	while (k<=n*n)
+	{
+		while(y<n&&!a[x][y+1])a[x][++y]=k++;
+		while(x<n&&!a[x+1][y])a[++x][y]=k++;
+		while(y>1&&!a[x][y-1])a[x][--y]=k++;
+		while(x>1&&!a[x-1][y])a[--x][y]=k++;
+	}
+	for (int i=1;i<=n;i++)
+	{
+		for (int j=1;j<=n;j++)printf("%d ",a[i][j]);
+		printf("\n");	
+	}	
+	return 0;
+} 
+*/
